7fabbfecf8d8027827087f040b4d9b1a815f1708
Illuminux <knut.welzel@googlemail.com>
2015-04-04 17:24:57 +0200

    Edit required software for Mac

--- include/system.sh
+++ include/system.sh
@@ -3,6 +3,28 @@
 
 
 FU_system_require_darwin() {
+
+	# required software for Mac
+	LV_requires=(
+		"gettext"
+		"gawk"
+		"grep"
+		"gnu-sed"
+		"curl"
+		"pkg-config"
+		"libtool"
+		"intltool"
+		"glib"
+		"intltool"
+#		"git"
+#		"curl"
+#		"autogen"
+#		"autoconf"
+#		"automake"
+#		"libtool"
+#		"bison"
+#		"cmake"
+	)
 	
 	# Check for Homebrew
 	if ! hash "brew" 2>/dev/null; then
@@ -20,45 +42,27 @@ FU_system_require_darwin() {
 	do
 		echo -n "Checking for '$require'... "
 		if [ $(brew list | grep -c $require) = 0 ]; then
+			echo
 			brew install $require
-			echo "no"
 		else 
 			echo "yes"
 		fi
 	done
 	
-	##Todo: link some force
+	# link gettext force
+	if ! hash gettext 2>/dev/null; then
+		brew link gettext --force
+	fi
+}
+
+FU_system_require_linux() {
+	
+	# required software for Linux
+	exit
 }
 
 
 FU_system_require() {
-
-	# required software
-
-	LV_requires=(
-		"git"
-		"gettext"
-		"curl"
-		"autogen"
-		"autoconf"
-		"automake"
-		"libtool"
-		"bison"
-		"cmake"
-		"gawk"
-		"glib"
-		"intltool"
-	)
-
-	
-	# search for required packages
-	if [ $GV_build_os = "Linux" ]; then 
-		exit
-		
-	elif [ $GV_build_os = "Darwin" ]; then
-		
-		FU_system_require_darwin
-	fi
 	
 	# Check if cross compiler is avalible
 	if ! [ -f "${UV_toolchain_dir}/bin/${UV_target}-gcc" ]; then
@@ -69,11 +73,15 @@ FU_system_require() {
 		exit 1
 	fi
 	
+	# search for required packages
+	if [ $GV_build_os = "Linux" ]; then 
+		FU_system_require_linux
+		
+	elif [ $GV_build_os = "Darwin" ]; then
+		
+		FU_system_require_darwin
+	fi
+	
 	unset LV_requires 
 	unset LV_missing_requires
-}
-
-FU_system_require_linux() {
-	
-	exit
 }
