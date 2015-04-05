d5a9d0ab3ca73ec5b95ab403a56392f28dfb8970
Illuminux <knut.welzel@googlemail.com>
2015-04-05 17:29:03 +0200

    Add debug and error heandling

--- formula/qjson.sh
+++ formula/qjson.sh
@@ -15,6 +15,10 @@ if [ $? == 1 ]; then
 	
 	FU_tools_check_depend
 	
+	GV_dir_name=${GV_dir_name%.*}
+	GV_name=$GV_dir_name
+	GV_build_start=`date +%s`
+	
 	GV_args=(
 		"-DCMAKE_SYSTEM_NAME='Linux'"
 		"-DCMAKE_SYSTEM_VERSION=1"
@@ -24,27 +28,18 @@ if [ $? == 1 ]; then
 		"-DCMAKE_FIND_ROOT_PATH='${CMAKE_FIND_ROOT_PATH} $UV_toolchain_dir'"
 		"-DCMAKE_INSTALL_PREFIX='$UV_sysroot_dir'"
 		"-DQT_QMAKE_EXECUTABLE='${UV_sysroot_dir}/Qt/bin/qmake'"
+		"${GV_source_dir}/${GV_dir_name}"
 	)
 	
-	GV_dir_name=${GV_dir_name%.*}
-	GV_name=$GV_dir_name
-	GV_build_start=`date +%s`
+	FU_file_git_clone
 	
-	FU_file_git_clone	
-	
-	echo -n "Configure ${GV_name}... "
+	# qjson has to be build in a seperate dir 
 	mkdir -p "${GV_source_dir}/${GV_dir_name}/build"
-	cd "${GV_source_dir}/${GV_dir_name}/build"
-	cmake ${GV_args[@]} "${GV_source_dir}/${GV_dir_name}" >$GV_log_file 2>&1
-	FU_tools_is_error "$?"
+	GV_dir_name="${GV_dir_name}/build"
 	
-	echo -n "Make ${GV_name}... "
-	make -j4 > $GV_log_file 2>&1
-	FU_tools_is_error "$?"
-
-	echo -n "Install ${GV_name}... "
-	make install >$GV_log_file 2>&1
-	FU_tools_is_error "$?"
+	FU_build_configure_cmake
+	FU_build_make
+	FU_build_install
 
 	FU_build_finishinstall
 fi
