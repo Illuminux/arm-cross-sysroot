64376ea3f8c1c0f0257069cc88f9e88d614848f9
Illuminux <knut.welzel@googlemail.com>
2015-05-02 18:05:11 +0200

    Dont save sha1 key into a file

--- tools/list_sha1.sh
+++ tools/list_sha1.sh
@@ -10,11 +10,11 @@ source "${GV_base_dir}/include/settings.cfg"
 for entry in `ls $UV_download_dir`; do
 	
 	if [ -f "${UV_download_dir}/${entry}" ]; then
-    	echo "Name: ${entry}" >> "./sha1-List.txt"
-		echo $(openssl sha1 "${UV_download_dir}/${entry}" | awk '{print $2}') >> "./sha1-List.txt"
-		echo >> "./sha1-List.txt"
+    	echo "Name: ${entry}" 
+		echo $(openssl sha1 "${UV_download_dir}/${entry}" | awk '{print $2}')
+		echo 
 	else
-		echo "Name: ${entry}"
+		echo "Name: ${entry} (Dir)"
 	fi
 	
 done
