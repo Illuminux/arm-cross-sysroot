48037f0f8215c5bd073af67dd9e76ce98b15f12f
Illuminux <knut.welzel@googlemail.com>
2015-05-09 10:32:38 +0200

    Edit Download URL

--- formula/lame.sh
+++ formula/lame.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GV_url="http://switch.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz"
+GV_url="http://softlayer-ams.dl.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz"
 GV_sha1="03a0bfa85713adcc6b3383c12e2cc68a9cfbf4c4"
 
 GV_depend=()
